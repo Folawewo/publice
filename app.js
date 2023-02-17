19796938d8691a97792842b2eaaca3718a804386
Folawewo Anthony <famwallexie@gmail.com>
2023-02-17 23:33:08 +0100

    added bodyParser

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const mongooase = require('mongoose');
 
 const app = express();
+
+app.use(bodyParser.json());
 
 module.exports = app;
