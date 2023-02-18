bdfaa18f1a3de9576e1051b6b88065caac057f6a
Folawewo Anthony <famwallexie@gmail.com>
2023-02-18 23:10:30 +0100

    database connected

--- services/mongo.js
+++ services/mongo.js
@@ -4,6 +4,8 @@ require('dotenv').config();
 
 const MONGO_URL = process.env.MONGO_URL;
 
+mongoose.set('strictQuery', false);
+
 mongoose.connect(MONGO_URL, () => {
   console.log('Database Connected');
 });
