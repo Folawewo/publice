4dbc7001d4dba8da0f19aabba6bdcf1cb6c05e7c
Folawewo Anthony <famwallexie@gmail.com>
2023-02-19 18:43:30 +0100

    get all posts

--- app.js
+++ app.js
@@ -8,4 +8,22 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.get('/', (req, res) => {
+  res.send('Hello, Welcome to PUBLICE!');
+});
+
+app.get('/posts', async (req, res) => {
+  try {
+    const posts = await BlogPost.find({});
+
+    if (posts.length === 0) {
+      return res.status(404).json({ error: 'No posts found!' });
+    }
+    res.json(posts);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Server error' });
+  }
+});
+
 module.exports = app;
